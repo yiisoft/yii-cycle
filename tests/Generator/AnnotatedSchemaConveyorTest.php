5bd4b50222ae49441f768cfacc9281fd9bee5e85
roxblnfk <roxblnfk@ya.ru>
2019-12-30 21:03:16 +0300

    Added AnnotatedSchemaConveyor test case

---
+++ tests/Generator/AnnotatedSchemaConveyorTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Yiisoft\Yii\Cycle\Tests\Generator;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Yii\Cycle\Generator\AnnotatedSchemaConveyor;
+use Yiisoft\Yii\Cycle\Tests\Generator\Stub\FakeContainer;
+use Yiisoft\Yii\Cycle\Tests\Generator\Stub\FakeGenerator;
+
+class AnnotatedSchemaConveyorTest extends TestCase
+{
+    public function testDefaultGeneratorsOrder()
+    {
+        $conveyor = $this->createConveyor();
+
+        // get generators list
+        /** @var string[] $generators */
+        $generators = array_map(
+            fn($value) => $value instanceof FakeGenerator ? $value->originClass() : get_class($value),
+            $conveyor->getGenerators()
+        );
+
+        $this->assertEquals([
+            'Cycle\Schema\Generator\ResetTables',
+            'Cycle\Annotated\Embeddings',
+            'Cycle\Annotated\Entities',
+            'Cycle\Annotated\MergeColumns',
+            'Cycle\Schema\Generator\GenerateRelations',
+            'Cycle\Schema\Generator\ValidateEntities',
+            'Cycle\Schema\Generator\RenderTables',
+            'Cycle\Schema\Generator\RenderRelations',
+            'Cycle\Annotated\MergeIndexes',
+            'Cycle\Schema\Generator\GenerateTypecast',
+        ], $generators);
+    }
+
+    public function testAddCustomGenerator()
+    {
+        $conveyor = $this->createConveyor();
+        $conveyor->addGenerator($conveyor::STAGE_USERLAND, \Cycle\Schema\Generator\SyncTables::class);
+
+        // get generators list
+        /** @var string[] $generators */
+        $generators = array_map(
+            fn($value) => $value instanceof FakeGenerator ? $value->originClass() : get_class($value),
+            $conveyor->getGenerators()
+        );
+
+        $this->assertEquals([
+            'Cycle\Schema\Generator\ResetTables',
+            'Cycle\Annotated\Embeddings',
+            'Cycle\Annotated\Entities',
+            'Cycle\Annotated\MergeColumns',
+            'Cycle\Schema\Generator\GenerateRelations',
+            'Cycle\Schema\Generator\ValidateEntities',
+            'Cycle\Schema\Generator\RenderTables',
+            'Cycle\Schema\Generator\RenderRelations',
+            'Cycle\Annotated\MergeIndexes',
+            'Cycle\Schema\Generator\SyncTables',
+            'Cycle\Schema\Generator\GenerateTypecast',
+        ], $generators);
+    }
+
+    public function createConveyor($entityPaths = ['@test-dir']): AnnotatedSchemaConveyor
+    {
+        $conveyor = new AnnotatedSchemaConveyor(new FakeContainer($this));
+        $conveyor->addEntityPaths($entityPaths);
+
+        return $conveyor;
+    }
+}
---
+++ tests/Generator/Stub/FakeContainer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Yiisoft\Yii\Cycle\Tests\Generator\Stub;
+
+use Cycle\Schema\GeneratorInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Yiisoft\Aliases\Aliases;
+
+class FakeContainer implements ContainerInterface
+{
+    private TestCase $testCase;
+    public function __construct(TestCase $testCase)
+    {
+        $this->testCase = $testCase;
+    }
+    public function get($id)
+    {
+        if ($id === Aliases::class) {
+            return new Aliases(['@test-dir' => __DIR__]);
+        }
+        if (is_a($id, GeneratorInterface::class, true)) {
+            return new FakeGenerator($id);
+        }
+        return $this->testCase->getMockBuilder($id)->getMock();
+    }
+    public function has($id)
+    {
+        return true;
+    }
+}
