cdd3afbe37a44c6cb861609b9b55115bf2e25164
roxblnfk <roxblnfk@ya.ru>
2020-01-14 15:22:06 +0300

    Promise factory now can be defined in `params.php` (#15)

--- src/Factory/OrmFactory.php
+++ src/Factory/OrmFactory.php
@@ -4,6 +4,7 @@ namespace Yiisoft\Yii\Cycle\Factory;
 
 use Cycle\ORM\Factory;
 use Cycle\ORM\ORM;
+use Cycle\ORM\PromiseFactoryInterface;
 use Cycle\ORM\Schema;
 use Psr\Container\ContainerInterface;
 use Spiral\Database\DatabaseManager;
@@ -26,6 +27,16 @@ final class OrmFactory
             $container->get(CycleOrmHelper::class)->getCurrentSchemaArray(true, $this->params['generators'] ?? [])
         );
 
-        return (new ORM(new Factory($dbal)))->withSchema($schema);
+        $orm = (new ORM(new Factory($dbal)))->withSchema($schema);
+        // Promise factory
+        $promiseFactory = $this->params['promiseFactory'] ?? null;
+        if ($promiseFactory) {
+            if (!$promiseFactory instanceof PromiseFactoryInterface) {
+                $promiseFactory = $container->get($promiseFactory);
+            }
+            $orm = $orm->withPromiseFactory($promiseFactory);
+        }
+
+        return $orm;
     }
 }
