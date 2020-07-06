f2c96d4108b47c75074e0ad821ac938d2f83020a
thenotsoft <44147615+thenotsoft@users.noreply.github.com>
2020-07-06 12:07:21 +0300

    remove unnecessary phpdoc block

--- src/Schema/SchemaProviderInterface.php
+++ src/Schema/SchemaProviderInterface.php
@@ -6,23 +6,15 @@ namespace Yiisoft\Yii\Cycle\Schema;
 
 interface SchemaProviderInterface
 {
-    /**
-     * @param array $config
-     * @return $this
-     */
     public function withConfig(array $config): self;
+    
     public function isWritable(): bool;
+    
     public function isReadable(): bool;
-    /**
-     * Read schema array
-     */
+    
     public function read(): ?array;
-    /**
-     * Write schema array
-     */
+    
     public function write(array $schema): bool;
-    /**
-     * Clear stored schema
-     */
+    
     public function clear(): bool;
 }
