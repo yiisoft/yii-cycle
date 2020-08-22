d5f481546c794150bc764437d5b97090097d42d9
Sergey Predvoditelev <sergey.predvoditelev@gmail.com>
2020-08-22 10:52:14 +0300

    Refactoring CoreFactory.

--- src/Factory/CoreFactory.php
+++ src/Factory/CoreFactory.php
@@ -4,22 +4,14 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Cycle\Factory;
 
-use Psr\Container\ContainerInterface;
 use Spiral\Core\FactoryInterface as SpiralFactoryInterface;
 use Yiisoft\Factory\Factory;
 
-final class CoreFactory implements SpiralFactoryInterface
+final class CoreFactory extends Factory implements SpiralFactoryInterface
 {
-
-    private $factory;
-
-    public function __construct(ContainerInterface $container)
-    {
-        $this->factory = new Factory($container);
-    }
 
     public function make(string $alias, array $parameters = [])
     {
-        return $this->factory->create($alias, $parameters);
+        return $this->create($alias, $parameters);
     }
 }
