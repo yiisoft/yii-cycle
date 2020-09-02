949ba76f405121e613c7cbdb4eb22e8cfaeca1e6
Alexander Makarov <sam@rmcreative.ru>
2020-09-02 18:27:24 +0300

    getType() -> gettype()

--- src/Exception/BadDeclarationException.php
+++ src/Exception/BadDeclarationException.php
@@ -10,7 +10,7 @@ class BadDeclarationException extends Exception
 {
     public function __construct(string $parameter, string $class, $argument)
     {
-        $type = is_object($argument) ? 'Instance of ' . get_class($argument) : ucfirst(getType($argument));
+        $type = is_object($argument) ? 'Instance of ' . get_class($argument) : ucfirst(gettype($argument));
         parent::__construct(sprintf(
             '%s should be instance of %s or its declaration. %s was received instead.',
             $parameter,
