a61f4885e51346486c564e55deeadb2668e29e85
roxblnfk <roxblnfk@ya.ru>
2020-09-04 15:16:27 +0300

    Remove FakeContainer; Refactor tests; Kill some mutants

--- tests/Schema/Conveyor/AnnotatedSchemaConveyorTest.php
+++ tests/Schema/Conveyor/AnnotatedSchemaConveyorTest.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Cycle\Tests\Schema\Conveyor;
 
-use PHPUnit\Framework\TestCase;
 use Yiisoft\Yii\Cycle\Exception\EmptyEntityPathsException;
 use Yiisoft\Yii\Cycle\Schema\Conveyor\AnnotatedSchemaConveyor;
-use Yiisoft\Yii\Cycle\Tests\Schema\Conveyor\Stub\FakeContainer;
 use Yiisoft\Yii\Cycle\Tests\Schema\Conveyor\Stub\FakeGenerator;
 
-class AnnotatedSchemaConveyorTest extends TestCase
+class AnnotatedSchemaConveyorTest extends BaseConveyorTest
 {
     public function testDefaultGeneratorsOrder(): void
     {
         $conveyor = $this->createConveyor();
 
-        // get generators list
-        /** @var string[] $generators */
-        $generators = array_map(
-            fn ($value) => $value instanceof FakeGenerator ? $value->originClass() : get_class($value),
-            $conveyor->getGenerators()
-        );
+        $generators = $this->getGeneratorClassList($conveyor);
 
         $this->assertSame([
             'Cycle\Schema\Generator\ResetTables',
@@ -42,12 +35,7 @@ class AnnotatedSchemaConveyorTest extends TestCase
         $conveyor = $this->createConveyor();
         $conveyor->addGenerator($conveyor::STAGE_USERLAND, \Cycle\Schema\Generator\SyncTables::class);
 
-        // get generators list
-        /** @var string[] $generators */
-        $generators = array_map(
-            fn ($value) => $value instanceof FakeGenerator ? $value->originClass() : get_class($value),
-            $conveyor->getGenerators()
-        );
+        $generators = $this->getGeneratorClassList($conveyor);
 
         $this->assertSame([
             'Cycle\Schema\Generator\ResetTables',
@@ -73,9 +61,31 @@ class AnnotatedSchemaConveyorTest extends TestCase
         $conveyor->getGenerators();
     }
 
+    public function testAnnotatedGeneratorsAddedOnlyOnce(): void
+    {
+        $conveyor = $this->createConveyor();
+
+        $conveyor->getGenerators();
+        $conveyor->getGenerators();
+        $generators = $this->getGeneratorClassList($conveyor);
+
+        $this->assertSame([
+            'Cycle\Schema\Generator\ResetTables',
+            'Cycle\Annotated\Embeddings',
+            'Cycle\Annotated\Entities',
+            'Cycle\Annotated\MergeColumns',
+            'Cycle\Schema\Generator\GenerateRelations',
+            'Cycle\Schema\Generator\ValidateEntities',
+            'Cycle\Schema\Generator\RenderTables',
+            'Cycle\Schema\Generator\RenderRelations',
+            'Cycle\Annotated\MergeIndexes',
+            'Cycle\Schema\Generator\GenerateTypecast',
+        ], $generators);
+    }
+
     public function createConveyor($entityPaths = ['@test-dir']): AnnotatedSchemaConveyor
     {
-        $conveyor = new AnnotatedSchemaConveyor(new FakeContainer($this));
+        $conveyor = new AnnotatedSchemaConveyor($this->prepareContainer());
         $conveyor->addEntityPaths($entityPaths);
 
         return $conveyor;
---
+++ tests/Schema/Conveyor/BaseConveyorTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Yii\Cycle\Tests\Schema\Conveyor;
+
+use Cycle\Schema\GeneratorInterface;
+use DateTime;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use Yiisoft\Aliases\Aliases;
+use Yiisoft\Test\Support\Container\Exception\NotFoundException;
+use Yiisoft\Test\Support\Container\SimpleContainer;
+use Yiisoft\Yii\Cycle\Exception\BadGeneratorDeclarationException;
+use Yiisoft\Yii\Cycle\Schema\SchemaConveyorInterface;
+use Yiisoft\Yii\Cycle\Tests\Schema\Conveyor\Stub\FakeGenerator;
+
+abstract class BaseConveyorTest extends TestCase
+{
+    public function badGeneratorProvider(): array
+    {
+        return [
+            [stdClass::class, '#Instance of ' . stdClass::class . '[\s\w]+instead#'],
+            [new DateTimeImmutable(), '#Instance of ' . DateTimeImmutable::class . ' [\s\w]+instead#'],
+            [fn () => new DateTime(), '#Instance of ' . DateTime::class . ' [\s\w]+instead#'],
+            [null, '#NULL [\s\w]+instead#'],
+            [42, '#Integer [\s\w]+instead#'],
+        ];
+    }
+
+    /**
+     * @dataProvider badGeneratorProvider
+     */
+    public function testAddWrongGenerator($badGenerator, string $message): void
+    {
+        $conveyor = $this->createConveyor();
+        $conveyor->addGenerator($conveyor::STAGE_USERLAND, $badGenerator);
+
+        $this->expectException(BadGeneratorDeclarationException::class);
+        $this->expectExceptionMessageMatches($message);
+
+        $conveyor->getGenerators();
+    }
+
+    protected function getGeneratorClassList(SchemaConveyorInterface $conveyor): array
+    {
+        return array_map(
+            fn ($value) => $value instanceof FakeGenerator ? $value->originClass() : get_class($value),
+            $conveyor->getGenerators()
+        );
+    }
+
+    protected function prepareContainer(): SimpleContainer
+    {
+        return new SimpleContainer([
+            stdClass::class => new stdClass(),
+            Aliases::class => new Aliases(['@test-dir' => __DIR__]),
+        ], function (string $id) {
+            if (is_a($id, GeneratorInterface::class, true)) {
+                return new FakeGenerator($id);
+            }
+            throw new NotFoundException($id);
+        });
+    }
+}
