fa79a9f10f0e0a89e6279549474573643ed86ba8
roxblnfk <roxblnfk@ya.ru>
2020-09-11 13:49:44 +0300

    Update migrator factory

--- src/Factory/MigratorFactory.php
+++ src/Factory/MigratorFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Yiisoft\Yii\Cycle\Factory;
 
 use Psr\Container\ContainerInterface;
+use Spiral\Core\FactoryInterface;
 use Spiral\Database\DatabaseManager;
 use Spiral\Migrations\Config\MigrationConfig;
 use Spiral\Migrations\FileRepository;
@@ -17,7 +18,11 @@ final class MigratorFactory
         $migConf = $container->get(MigrationConfig::class);
         $dbal = $container->get(DatabaseManager::class);
 
-        $migrator = new Migrator($migConf, $dbal, new FileRepository($migConf));
+        $migrator = new Migrator(
+            $migConf,
+            $dbal,
+            new FileRepository($migConf, $container->get(FactoryInterface::class))
+        );
         // Init migration table
         $migrator->configure();
         return $migrator;
