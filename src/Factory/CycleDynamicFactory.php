52982dac7b0fbb6421b858ce1ed3e60a5f6cb219
Aleksei Gagarin <roxblnfk@ya.ru>
2020-10-08 01:11:15 +0300

    Replace yiisoft/factory to yiisoft/injector for the CycleDynamicFactory (#64)

--- src/Factory/CycleDynamicFactory.php
+++ src/Factory/CycleDynamicFactory.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Cycle\Factory;
 
+use Psr\Container\ContainerInterface;
 use Spiral\Core\FactoryInterface;
-use Yiisoft\Factory\Factory;
+use Yiisoft\Injector\Injector;
 
-final class CycleDynamicFactory extends Factory implements FactoryInterface
+final class CycleDynamicFactory implements FactoryInterface
 {
+    private Injector $injector;
+    public function __construct(ContainerInterface $container)
+    {
+        $this->injector = new Injector($container);
+    }
+
     public function make(string $alias, array $parameters = [])
     {
-        return $this->create($alias, $parameters);
+        return $this->injector->make($alias, $parameters);
     }
 }
