061b4667e589d7fed8d14cd1a4ce19dcd204b4f2
Aleksei Gagarin <roxblnfk@ya.ru>
2021-04-12 00:32:04 +0300

    Fix config

--- config/common.php
+++ config/common.php
@@ -16,7 +16,7 @@ use Yiisoft\Yii\Cycle\Exception\SchemaWasNotProvidedException;
 use Yiisoft\Yii\Cycle\Factory\CycleDynamicFactory;
 use Yiisoft\Yii\Cycle\Factory\DbalFactory;
 use Yiisoft\Yii\Cycle\Factory\OrmFactory;
-use Yiisoft\Yii\Cycle\Schema\Conveyor\AnnotatedSchemaConveyor;
+use Yiisoft\Yii\Cycle\Schema\Conveyor\CompositedSchemaConveyor;
 use Yiisoft\Yii\Cycle\Schema\Provider\Support\SchemaProviderPipeline;
 use Yiisoft\Yii\Cycle\Schema\SchemaConveyorInterface;
 use Yiisoft\Yii\Cycle\Schema\SchemaProviderInterface;
@@ -42,8 +42,7 @@ return [
         return new Factory(
             $container->get(DatabaseManager::class),
             null,
-            $container->get(SpiralFactoryInterface::class),
-            $container
+            $container->get(SpiralFactoryInterface::class)
         );
     },
     // Schema provider
@@ -60,7 +59,7 @@ return [
     },
     // Annotated Schema Conveyor
     SchemaConveyorInterface::class => static function (ContainerInterface $container) use (&$params) {
-        $conveyor = new AnnotatedSchemaConveyor($container);
+        $conveyor = new CompositedSchemaConveyor($container);
         $conveyor->addEntityPaths($params['yiisoft/yii-cycle']['annotated-entity-paths']);
         return $conveyor;
     },
