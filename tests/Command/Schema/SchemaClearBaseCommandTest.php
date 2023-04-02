e0ea1ca41d8c14a15b4fa08e43578d28960b3e02
Gordienko Vladislav <gordienkovladis@gmail.com>
2023-04-02 11:08:44 +0500

    remove used method

--- tests/Command/Schema/SchemaClearBaseCommandTest.php
+++ tests/Command/Schema/SchemaClearBaseCommandTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Cycle\Tests\Command\Schema;
 
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Yiisoft\Test\Support\Container\SimpleContainer;
@@ -12,7 +13,7 @@ use Yiisoft\Yii\Cycle\Command\CycleDependencyProxy;
 use Yiisoft\Yii\Cycle\Command\Schema\SchemaClearCommand;
 use Yiisoft\Yii\Cycle\Schema\SchemaProviderInterface;
 
-class SchemaClearBaseCommandTest extends \PHPUnit\Framework\TestCase
+class SchemaClearBaseCommandTest extends TestCase
 {
     public function testExecute(): void
     {
@@ -27,13 +28,5 @@ class SchemaClearBaseCommandTest extends \PHPUnit\Framework\TestCase
         $code = $command->run($input, $output);
 
         $this->assertEquals(ExitCode::OK, $code);
-    }
-
-    protected function getContainerDefinitions(): array
-    {
-        $schemaProvider = $this->getMockBuilder(SchemaProviderInterface::class)->getMock();
-        $schemaProvider->expects($this->once())->method('clear');
-
-        return [SchemaProviderInterface::class => $schemaProvider];
     }
 }
