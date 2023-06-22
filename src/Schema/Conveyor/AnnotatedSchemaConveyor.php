08668117fd38c4efdf9fe4fa697855101eebaf0e
Nikolay Churkin <34393267+Kutuzovska@users.noreply.github.com>
2023-06-22 20:27:45 +0300

    Remove deprecated function registerLoader (#167)

--- src/Schema/Conveyor/AnnotatedSchemaConveyor.php
+++ src/Schema/Conveyor/AnnotatedSchemaConveyor.php
@@ -8,22 +8,6 @@ use Spiral\Attributes\AnnotationReader;
 
 final class AnnotatedSchemaConveyor extends MetadataSchemaConveyor
 {
-    private bool $isAutoloadRegistered = false;
-
-    public function getGenerators(): array
-    {
-        if (!$this->isAutoloadRegistered) {
-            /**
-             * autoload annotations
-             *
-             * @psalm-suppress DeprecatedMethod
-             */
-            $this->isAutoloadRegistered = true;
-        }
-
-        return parent::getGenerators();
-    }
-
     protected function getMetadataReader(): AnnotationReader
     {
         return new AnnotationReader();
