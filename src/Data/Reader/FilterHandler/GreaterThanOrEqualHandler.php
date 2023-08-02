ffcb82bdcb240eecf80590ec6e99cb304e855cd9
Nikolay Churkin <nc19091997@gmail.com>
2023-08-02 22:46:56 +0300

    Hot fix

--- src/Data/Reader/FilterHandler/GreaterThanOrEqualHandler.php
+++ src/Data/Reader/FilterHandler/GreaterThanOrEqualHandler.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\Cycle\Data\Reader\FilterHandler;
 
-use Yiisoft\Data\Reader\Filter\GreaterThan;
+use Yiisoft\Data\Reader\Filter\GreaterThanOrEqual;
 
 final class GreaterThanOrEqualHandler extends CompareHandler
 {
     public function getOperator(): string
     {
-        return GreaterThan::getOperator();
+        return GreaterThanOrEqual::getOperator();
     }
 
     protected function getSymbol(): string
