339785ca9818f7bb0f2ec992fc79a63a1261a1ea
Rustam <rmamdaminov@gmail.com>
2025-07-29 11:32:24 +0500

    Fix #227: finalize `ConfigException`

--- src/Exception/ConfigException.php
+++ src/Exception/ConfigException.php
@@ -6,11 +6,7 @@ namespace Yiisoft\Yii\Cycle\Exception;
 
 use Exception;
 
-/**
- * @final Will be marked as final in next major version
- * @todo Remove this note and make the class final
- */
-class ConfigException extends Exception
+final class ConfigException extends Exception
 {
     /**
      * @param string[] $section Config path
